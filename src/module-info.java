e42acf593637618100143cd070949c5b52c67849
pranali <apmosys@administrator-Lenovo-E41-55>
2023-07-27 15:07:26 +0530

    Message Display

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author apmosys
+ *
+ */
+module jenkinsDemoPro {
+}
---
+++ src/program/helloAdd.java
@@ -0,0 +1,17 @@
+package program;
+
+public class helloAdd {
+	
+public static void main(String[] args)
+
+{
+	int a=10;
+	int b=20;
+	int c=a+b;
+	
+	System.out.println("Hello World!!!!!");
+	System.out.println(c);
+	
+}
+
+}
